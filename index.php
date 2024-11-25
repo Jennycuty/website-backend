b4490b9731ac9b1f8d5b297877191d4d9ae5945a
Jennycuty <Jennyngozi90.gmail.com>
2024-11-25 14:44:41 +0100

    all codes in backend class

---
+++ index.php
@@ -0,0 +1,313 @@
+<?php
+// function printstateAndcapital(){
+//  $stateAndcapital(){
+
+
+//     </table>
+
+
+//     <Script
+    
+// echo "my webside <br>";
+
+//function
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//$a +=$b;
+    
+    //subtraction assignment
+//$a -= $b;
+
+//echo "<br>";
+
+
+// PHP conditional statements
+// there are four notable conditional statement 
+
+// IF STATEMENT 
+
+// else if statement 
+
+// switch statement 
+ 
+//__________________
+
+// the if statement is one of the mostimportant statement in php
+//example
+//_____________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ///
+
+//--do...while loops
+
+//--for -loops
+//}
+
+
+   
+
+
+
+
+
+
+// 11/11/2024 
+ //php loops
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //*do while loop
+ /*$numbers =11;
+ do{
+ echo $numbers."<br>";
+ }while ($numbers <+10) */
+ // for loop
+//  for ( $numbers + 1; $numbers <+100;$number**)  {
+//     if ( $numbers % 3 == 0){ echo "fizz <br>;"
+//         echo 
+//         echo $numbers."<br>;    
+//         echo fizzbuzz <br>;"  
+
+
+
+
+PS C;\XAMP\htdoc\website-backend>git init 
+
+
+
+
+
+
+git remote add origin https://github.com/Jennycuty/website-backend.git
+
+git branch -M main
+git push -u origin main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var_dump($phone);
+// // backend 
+// // index
+// //$fullname=array("evans" ,"jennifer" ," edike")
+// //associative array
+// $fullname =array ("firstname" 
+// <table class="table table-striprd">
+// if (count ($stateAndcapitals)>)0 
+// foreach($stateAndcapital as $key =$value 
+
+// else
+// echo"
+// <tr>
+// <tr>nostate</td>
+// <td>no capital</td>>
+// </tr>
+// ";
+// )$stateAndcapital
+// "Abia Umuahia
+// Adamawa yola 
+// Bauchi "
+// // function 
+echo "<br> good afternoon";
+
+function greetme() {
+    echo "<br> good evening";
+}
+
+greetme();
+
+
+?> 
+
+
+
